437158850ccaf18b839079417339729fc47e240b
2021BTech <daffydanni@gmail.com>
2022-09-01 11:38:39 +0100

    Set-up

---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Experience.css";
+
+const Experience = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Experience
---
+++ src/components/Testify/Testify.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Testify.css";
+
+const Testify = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Testify
