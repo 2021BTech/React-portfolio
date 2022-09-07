bf5c3498a4ca277872bfe82c6f36ca92692a5b71
2021BTech <daffydanni@gmail.com>
2022-09-07 14:38:24 +0100

    Portfolio & testimony

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,12 +1,83 @@
 import React from 'react';
 import "./Portfolio.css";
+import IMG1 from "../../asset/portfolio 1.jpg"
+import IMG2 from "../../asset/portfolio 2.png"
+import IMG3 from "../../asset/portfolio 3.jpg"
+import IMG4 from "../../asset/portfolio 4.png"
+import IMG5 from "../../asset/portfolio 51.png"
+import IMG6 from "../../asset/portfolio 61.png"
+
+const data = [
+    {
+        id:1,
+        image:IMG1,
+        title:"OminiFoods",
+        github:"https://github.com/2021BTech?tab=overview&from=2022-09-01&to=2022-09-02",
+        demo:"https://github.com/2021BTeominFoodsch/"
+    },
+  {
+    id:2,
+    image:IMG2,
+    title:"Portfolio- with React jS",
+    github:"https://github.com/2021BTech?tab=overview&from=2022-09-01&to=2022-09-02",
+    demo:"https://github.com/2021BTeominFoodsch/"
+  },
+  {
+    id:3,
+    image:IMG3,
+    title:"Crunchy Movies with React",
+    github:"https://github.com/2021BTech?tab=overview&from=2022-09-01&to=2022-09-02",
+    demo:"https://github.com/2021BTeominFoodsch/"
+  },
+  {
+    id:4,
+    image:IMG4,
+    title:"Chat-bot with React/Socket.io",
+    github:"https://github.com/2021BTech?tab=overview&from=2022-09-01&to=2022-09-02",
+    demo:"https://github.com/2021BTeominFoodsch/"
+  },
+  {
+    id:5,
+    image:IMG5,
+    title:"Rest countries API",
+    github:"https://github.com/2021BTech?tab=overview&from=2022-09-01&to=2022-09-02",
+    demo:"https://github.com/2021BTeominFoodsch/"
+  },
+  {
+    id:6,
+    image:IMG6,
+    title:"GVE DEMO-Property App",
+    github:"https://github.com/2021BTech?tab=overview&from=2022-09-01&to=2022-09-02",
+    demo:"https://github.com/2021BTeominFoodsch/"
+  }
+]
 
 const Portfolio = () => {
   return (
     <section id='Portfolio'>
-        Portfolio
+        <h5> My Recent Work </h5>
+        <h2> Portfolio </h2>
+
+        <div className='container portfolio__container'>
+          {
+            data.map(({id, image, title, github, demo}) => {
+              return (
+                <article key={id} className='portfolio__item'>
+                  <div className='portfolio__item-image'>
+                    <img src={image} alt={title} />
+                  </div>
+                    <h3> {title} </h3>
+                    <div className='portfolio__item-cta'>
+                      <a href={github} className='btn'> GITHUB </a>
+                      <a href={demo} className='btn btn-primary' target="_blank"> Live Demo</a>
+                    </div>
+                </article>
+              )
+            })
+          }
+        </div>
     </section>
-  )
+   )
 }
 
 export default Portfolio
--- src/components/Testify/Testify.jsx
+++ src/components/Testify/Testify.jsx
@@ -1,10 +1,72 @@
 import React from 'react';
 import "./Testify.css";
+import AV1 from '../../asset/Avatar 1.jpg';
+import AV2 from '../../asset/Avatar 2.jpg';
+import AV3 from '../../asset/Avatar 3.jpg';
+import AV4 from '../../asset/Avatar 4.jpg';
+
+// import Swiper core and required modules
+import {  Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+const data = [
+  {
+    avatar: AV1,
+    name: "BonitasICT",
+    reveiw: "Briefly but regularly reviewing how you work will help you to promote clearer thinking in the work itself. Daniel is detailed oriented and has good work ethics."
+  },
+
+  {
+  avatar: AV2,
+  name: "Schiefersten Morgun",
+  reveiw: "I don't believe that man was born to work for a living; I believe he was born to make what he lives for HIS WORK. Daniel's work speaks for him...you won't regret working with him"
+  }, 
+
+  {
+    avatar: AV3,
+    name: "Hemphrey Berenice",
+    reveiw: ". . . there is an exciting world of possibilities out there that very few of us get the chance to explore during our working lives, but retirement is that time!"
+  },
+
+  {
+  avatar: AV4,
+  name: "Emsley Roseanna",
+  reveiw: "Laughter in the workplace leads to employees who are genuinely happy to go to work every day. Talking to Daniel each day brings it's own form of development.Test and see!"
+  }
+]
 
 const Testify = () => {
   return (
     <section id='Testify'>
-      Testify
+      <h5> Review from client </h5>
+      <h2> Testimonials </h2>
+      <Swiper className='container testify__container'
+      // install Swiper modules
+      modules={[Pagination]}
+      spaceBetween={40}
+      slidesPerView={1}
+      navigation
+      pagination={{ clickable: true }}
+      >
+        {
+          data.map(({avatar, name, reveiw}, index) =>{
+            return (
+              <SwiperSlide key={index} className='testify'>
+                <div className='client__avatar'>
+                  <img src={avatar} alt='Image1'/>
+                </div>
+                <h5 className='client__name'>{name}</h5>
+                <small className='client__review'>{reveiw}</small>
+            </SwiperSlide>
+            )
+          })
+        }
+      </Swiper>
     </section>
   )
 }
